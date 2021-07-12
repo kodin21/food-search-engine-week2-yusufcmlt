88760a2e94631f3dd17ffbb135ea088e4abfc8b2
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-12 22:28:16 +0300

    some comment changes

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ import Loading from "./src/components/Loading";
 import { getUserName, getFoodData } from "./src/services/api-service";
 
 //Selecting elements
-
 const headerElement = document.getElementById("JSheader"); //header inside body   ==> search will be inside and results will be inserted after.
 
-//Render loading element on first load.
+//Render loading element on first load. ---> inserts into header element
 Loading(headerElement);
 
-//Get User data and remove loading
-//Added 1 second timeout just for showing loading animation because it gets data fast and you can't see the loading.
-const randomUser = Math.floor(Math.random() * 10) + 1; //Random user every time document loads (user id  1-10)
+const randomUserID = Math.floor(Math.random() * 10) + 1; //Random user id every time document loads (user id  1-10)
+
+//Get User data and remove loading --->inserts into header element
+//Added 600ms timeout just for showing loading animation because it gets data fast and you can't see the loading.
 setTimeout(() => {
-  getUserName(randomUser)
+  getUserName(randomUserID)
     .then((data) => {
       headerGreet(headerElement, data.name);
     })
     .catch(() => {
       headerGreet(headerElement);
     });
-}, 1000);
+}, 600);
