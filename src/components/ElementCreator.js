d6a578882a8164458a292b982d97184bfa5d7ae6
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-12 12:04:53 +0300

    html element creator function

--- src/components/ElementCreator.js
+++ src/components/ElementCreator.js
@@ -1,1 +1,20 @@
-function ElementCreator(currentDocument) {}
+//HTML Element Creator with given arguments.
+//Name:element name
+//Attributes: class,id,type,name etc.
+
+//ElementCreator('li',{''name'':'something','class':'something-else'})
+
+const elementCreator = (name = "div", attributes = {}, textInside = "") => {
+  const htmlElement = document.createElement(name);
+  //Checking if attributes object has any property
+  if (Object.keys(attributes).length) {
+    for (let key in attributes) {
+      htmlElement.setAttribute(key, attributes[key]);
+    }
+  }
+
+  htmlElement.textContent = textInside;
+
+  return htmlElement;
+};
+export default elementCreator;
