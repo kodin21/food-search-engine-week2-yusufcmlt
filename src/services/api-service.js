73c27a7e326b49ccd8e3eb33f4439e5d658b2816
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-12 12:04:26 +0300

    get user name from api

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,4 +1,17 @@
-const USER_API_URL = "https://jsonplaceholder.typicode.com/users/1";
+import axios from "axios";
+
+const USER_API_URL = "https://jsonplaceholder.typicode.com/users/";
 const MEAL_API_URL = "www.themealdb.com/api/json/v1/1/search.php?s=";
 
-function getUserName() {}
+const getUserName = async (userID = "1") => {
+  try {
+    const userData = await axios.get(`${USER_API_URL}${userID}`);
+    return userData.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getFoodData = async () => {};
+
+export { getUserName, getFoodData };
