4e7bcf978b93d7b37ccd97b93e1b8cf3d0482cc1
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-12 12:05:33 +0300

    fix loading element

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,15 +1,22 @@
 //This component is for showing a placeholder while getting/rendering any data.
 //Just for visuals, has no functional purpose
 
+import elementCreator from "./ElementCreator";
+import loadingIcon from "../img/icons/pizza-loading.svg";
+
 //Styles in style.css
 
 function Loading() {
-  return `<div class="loading">
-    <img
-      src="./src/img/icons/pizza-loading.svg"
-      alt="loading icon"
-      class="loading__icon"
-    />
-    <h2 class="loading__text">Yükleniyor...</h3>
-  </div>`;
+  const loadingElement = elementCreator("div", { class: "loading" });
+
+  loadingElement.innerHTML = `<img
+ src="${loadingIcon}"
+ alt="loading icon"
+ class="loading__icon"
+/>
+<h2 class="loading__text">Yükleniyor...</h3>`;
+
+  return loadingElement;
 }
+
+export default Loading;
