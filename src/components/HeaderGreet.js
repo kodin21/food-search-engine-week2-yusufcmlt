28ab9ece5ae8b414fef132bf28fc49dcf0c4ab98
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-12 22:26:41 +0300

    create suggestions box

--- src/components/HeaderGreet.js
+++ src/components/HeaderGreet.js
@@ -4,15 +4,21 @@
 import mealPlateImg from "../img/food-bowl-header.png";
 import { getFoodSuggestions } from "../services/api-service";
 import { debounce } from "../services/utils";
+import {
+  createSuggestionsList,
+  removeSuggestionsList,
+} from "./SearchSuggestions";
 
 export default function headerGreet(rootHeader, userName = "Human") {
   rootHeader.innerHTML = `<div class="header__greet">
     <h1 class="header__heading">
       Hello <span class="header__heading--username">${userName}!</span>
     </h1>
-    <form class="header__search-container">
+    <form id="JSformElement" class="header__search-container">
       <input
+      autocomplete="off"
         id="JSsearchInput"
+        name="searchInput"
         type="text"
         placeholder="Search for the meal on your mind. "
         class="header__search-input"
@@ -29,9 +35,14 @@ export default function headerGreet(rootHeader, userName = "Human") {
     />
   </div>`;
 
-  //Selecting search box input and adding event listener.
+  //Selecting search box input and form element.
+  const formElement = document.getElementById("JSformElement");
   const searchInput = document.getElementById("JSsearchInput");
+
+  //Typing in search box
+  //Debounce ---->   Delay the api call until user stops typing.
   searchInput.addEventListener("input", debounce(handleSearch));
+  formElement.addEventListener("submit", handleSubmit);
 
   //Modifying input for api
   //Hello world -----> 'hello+world'
@@ -40,21 +51,40 @@ export default function headerGreet(rootHeader, userName = "Human") {
   }
 
   //Input event
-  async function handleSearch(event) {
-    const { value } = event.target;
+  async function handleSearch() {
+    //There is only one input (search box) so we don't need select value-
+    //- with event target etc.
+    const value = searchInput.value;
     const modifiedSearch = modifySearch(value);
 
-    //if search box has a value -or not only empty spaces-
+    //if search box has a value -and not only empty spaces-
     if (modifiedSearch) {
       //Get search results
       const suggestionData = await getFoodSuggestions(modifiedSearch);
       const { meals } = suggestionData;
+
       //Check if response data has any results
       if (meals) {
-        //Get only names as suggestions
-        const suggestionResults = meals.map((suggestion) => suggestion.strMeal);
-        //console.log(suggestionResults);
+        //Get only names as suggestions with the limit of 5 items at most
+        const suggestionResults = meals
+          .map((suggestion) => suggestion.strMeal)
+          .slice(0, 5);
+
+        //Create suggestion items box with given suggestion array inside given form, its position already set in style.css
+        createSuggestionsList(formElement, suggestionResults);
+      } else {
+        //No result no box.
+        removeSuggestionsList();
       }
+    } else {
+      //No search input no box.
+      removeSuggestionsList();
     }
   }
+}
+
+//Searching
+//Clicking a suggestion or typing and entering
+async function handleSubmit(event) {
+  event.preventDefault();
 }
--- src/components/SearchSuggestions.js
+++ src/components/SearchSuggestions.js
@@ -1,5 +1,28 @@
 //Search suggestions with given suggestion array.
+//Inserts right after search box that contained by form
+let suggestionsList = document.getElementById("JSsuggestionsList");
 
-function SearchSuggestions(suggestionData) {
-  return ``;
+function createSuggestionsList(suggestionRoot, suggestionItems) {
+  if (suggestionsList) {
+    removeSuggestionsList();
+  }
+
+  suggestionsList = document.createElement("ul");
+  suggestionsList.setAttribute(
+    "class",
+    "search__suggestions search__suggestions--active"
+  );
+  suggestionsList.setAttribute("id", "JSsuggestionsList");
+
+  suggestionItems.forEach((item) => {
+    suggestionsList.innerHTML += `<li class="suggestion">${item}</li>`;
+  });
+
+  suggestionRoot.appendChild(suggestionsList);
 }
+
+function removeSuggestionsList() {
+  suggestionsList.remove();
+}
+
+export { createSuggestionsList, removeSuggestionsList };
