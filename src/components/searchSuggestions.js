e2ef949d6617c855e9d9f324bd93f08e5e8d2f8a
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-14 18:45:06 +0300

    create search suggestions component

--- src/components/searchSuggestions.js
+++ src/components/searchSuggestions.js
@@ -1,1 +1,30 @@
-export default function createSuggestions() {}
+import { parseHTML } from "../services/utils";
+
+/**
+ * Aramaya icerigine gore getirilen onerilerin gorsellestirilmesi:
+ *
+ * createSuggestionList gelen listeye gore ul olusturur ve -
+ * createSuggestionElement gelen liste elemanina gore olusturulan elemanlar-
+ * ul icerisine eklenir.
+ *
+ */
+
+export default function createSuggestionList(searchResultList) {
+  const suggestionListMarkup = ` <ul class="search__suggestions"></ul>`;
+  const suggestionListElement = parseHTML(suggestionListMarkup);
+
+  //Gelen listeye gore bir liste elemani olustur
+  searchResultList.forEach((listItem) => {
+    suggestionListElement.appendChild(createSuggestionElement(listItem));
+  });
+
+  return suggestionListElement;
+}
+
+//Gelen oneri ismine gore liste elemani olustur
+function createSuggestionElement(suggestionName) {
+  const suggestionMarkup = `<li class="suggestion">${suggestionName}</li>`;
+  const suggestionElement = parseHTML(suggestionMarkup);
+
+  return suggestionElement;
+}
