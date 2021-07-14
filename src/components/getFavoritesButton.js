de373d42096f530bf3a04bfb380e983ea97217dd
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-14 18:04:55 +0300

    create button for getting favorites

--- src/components/getFavoritesButton.js
+++ src/components/getFavoritesButton.js
@@ -1,1 +1,9 @@
-export default function createFavoriteButton() {}
+import { parseHTML } from "../services/utils";
+
+export default function createFavoriteButton() {
+  const buttonMarkup = `<button id="JSfavoriteLink" class="header__favorite-link">My favorite meals</button>`;
+
+  const buttonElement = parseHTML(buttonMarkup);
+
+  return buttonElement;
+}
