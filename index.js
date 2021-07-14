39b6747ff1b9117e7272fc9c03b03dcdf84c6d9a
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-14 18:44:51 +0300

    small naming changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "regenerator-runtime/runtime";
 import createHeader from "./src/components/header";
 
 import { getUser } from "./src/services/api-service";
+import { fuzzySearch } from "./src/services/utils";
 
 //Ana elemanlarin secimesi (Header ve Sonuc bolumu)
 const headerElement = document.getElementById("JSheader");
@@ -10,13 +11,14 @@ const resultsSection = document.getElementById("JSsearchResults");
 //Rastgele bir user id olustur
 const randomUserID = Math.floor(Math.random() * 10) + 1;
 
-const loadHeader = async () => {
+const loadApp = async () => {
   //Kullaniciyi getir
   const userData = await getUser(randomUserID);
 
   //Gelen kullanici ismi ile headeri olustur.
   createHeader(userData.name);
+  //Header olusturulduktan sonra sayfa icerigi header tarafindan yonetilir.
 };
 
 //Sayfa yuklenme durumunda headeri getir.
-window.addEventListener("DOMContentLoaded", loadHeader);
+window.addEventListener("DOMContentLoaded", loadApp);
