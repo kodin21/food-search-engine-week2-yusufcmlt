a10cc6cce4a45471d2483bd6135bbe3cfd32be04
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-15 01:10:01 +0300

    create search results section

---
+++ src/components/searchResults.js
@@ -0,0 +1,8 @@
+//Verilen sonuclar ile sonuc sayfasi(bolumu) olusturma;
+//Gelen array durumuna gore.
+
+export default function createSearchResult(mealList) {
+  console.log(mealList);
+}
+
+function createmealCard(cardData) {}
