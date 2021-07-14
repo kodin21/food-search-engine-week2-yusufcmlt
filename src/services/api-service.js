17bc6f864b2c76a5a967bc31f31bd1527359fdd8
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-14 21:21:18 +0300

    move fuzzy search

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+import Fuse from "fuse.js";
+
+import mealsList from "./mealsmin.js";
 
 const USER_API_URL = "https://jsonplaceholder.typicode.com/users/";
-const MEAL_API_URL = "www.themealdb.com/api/json/v1/1/search.php?s=";
+//const MEAL_API_URL = "www.themealdb.com/api/json/v1/1/search.php?s="; --->KULLANILMIYOR
 
 //Verilen user id ile kullanici ismi getir.
 const getUser = async (userID) => {
@@ -9,4 +12,12 @@ const getUser = async (userID) => {
   return userData.data;
 };
 
-export { getUser };
+//Fuse.js ile fuzzy arama yapma
+//mealsmin.js icerisinde yemek isimlerinden arama yapma
+const fuse = new Fuse(mealsList, { keys: ["strMeal"] });
+const fuzzySearch = async (searchTerm) => {
+  const resultData = await fuse.search(searchTerm);
+  return resultData;
+};
+
+export { getUser, fuzzySearch };
