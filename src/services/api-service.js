0a534387f7e9dfaa34afc0a09c2700c25b051451
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-14 17:09:33 +0300

    implement getUser func

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,4 +1,12 @@
-const USER_API_URL = "https://jsonplaceholder.typicode.com/users/1";
+import axios from "axios";
+
+const USER_API_URL = "https://jsonplaceholder.typicode.com/users/";
 const MEAL_API_URL = "www.themealdb.com/api/json/v1/1/search.php?s=";
 
-function getUserName() {}
+//Verilen user id ile kullanici ismi getir.
+const getUser = async (userID) => {
+  const userData = await axios.get(`${USER_API_URL}${userID}`);
+  return userData.data;
+};
+
+export { getUser };
