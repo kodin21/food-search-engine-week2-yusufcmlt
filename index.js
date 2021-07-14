b9e59ede185bc553e35e6b0b2ad5d766d86ffbbb
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-14 17:09:07 +0300

    create header

---
+++ index.js
@@ -0,0 +1,22 @@
+import "regenerator-runtime/runtime";
+import createHeader from "./src/components/header";
+
+import { getUser } from "./src/services/api-service";
+
+//Ana elemanlarin secimesi (Header ve Sonuc bolumu)
+const headerElement = document.getElementById("JSheader");
+const resultsSection = document.getElementById("JSsearchResults");
+
+//Rastgele bir user id olustur
+const randomUserID = Math.floor(Math.random() * 10) + 1;
+
+const loadHeader = async () => {
+  //Kullaniciyi getir
+  const userData = await getUser(randomUserID);
+
+  //Gelen kullanici ismi ile headeri olustur.
+  createHeader(headerElement, userData.name);
+};
+
+//Sayfa yuklenme durumunda headeri getir.
+window.addEventListener("DOMContentLoaded", loadHeader);
