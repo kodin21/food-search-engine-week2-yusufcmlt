d9e6bec720b324b3dac32185ff3afc7deac8569a
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-14 22:12:44 +0300

    get suggestions

--- src/components/searchForm.js
+++ src/components/searchForm.js
@@ -1,4 +1,7 @@
-import { parseHTML } from "../services/utils";
+import { fuzzySearch } from "../services/api-service";
+import { debounce, parseHTML } from "../services/utils";
+
+import createSuggestionList from "./searchSuggestions";
 
 export default function createSearchForm() {
   const formMarkup = `
@@ -15,6 +18,27 @@ export default function createSearchForm() {
   const formElement = parseHTML(formMarkup);
   const searchInput = formElement.querySelector("input");
   const searchButton = formElement.querySelector("button");
+
+  //Elemanlara event ekleme
+  //Arama kismina yazma durumunda debounce uygula
+  //Arama tusuna basma veya enter durumunda sonuclari renderla
+  formElement.addEventListener("submit", handleSubmitSearch);
+  searchInput.addEventListener("input", debounce(handleGetSuggestions));
+
+  //Arama kutusunda girilen terimlere gore oneri getirilmesi
+  async function handleGetSuggestions(event) {
+    console.log(event.target.value);
+    fuzzySearch(event.target.value).then((mealData) => {
+      //Sadece ilk 5 oneriyi getir.
+      mealData = mealData.slice(0, 5);
+
+      formElement.appendChild(createSuggestionList(mealData));
+    });
+  }
+
+  function handleSubmitSearch(event) {
+    event.preventDefault();
+  }
 
   return formElement;
 }
