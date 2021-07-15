095ea173ca8f1694e05d7407a0673f9c4e8b2a73
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-15 15:43:43 +0300

    get one meal from suggestion

--- src/components/searchSuggestions.js
+++ src/components/searchSuggestions.js
@@ -1,4 +1,6 @@
+import { fuzzySearch } from "../services/api-service";
 import { parseHTML } from "../services/utils";
+import createSearchResult from "./searchResults";
 
 /**
  * Aramaya icerigine gore getirilen onerilerin gorsellestirilmesi:
@@ -11,10 +13,7 @@ import { parseHTML } from "../services/utils";
 
 export default function createSuggestionList(searchResultList) {
   //Eski listeyi (eger varsa) sil
-  const oldSuggestionsList = document.getElementById("JSsuggestionsList");
-  if (oldSuggestionsList) {
-    oldSuggestionsList.remove();
-  }
+  removeSuggestionList();
   //Liste container olusturulmasi
   const suggestionListMarkup = ` <ul id="JSsuggestionsList" class="search__suggestions"></ul>`;
   const suggestionListElement = parseHTML(suggestionListMarkup);
@@ -29,8 +28,35 @@ export default function createSuggestionList(searchResultList) {
 
 //Gelen oneri ismine gore liste elemani olustur
 function createSuggestionElement(suggestionName) {
+  //Gereken elemanlarin olusturulmasi/secimi.
   const suggestionMarkup = `<li class="suggestion">${suggestionName}</li>`;
   const suggestionElement = parseHTML(suggestionMarkup);
+  const searchInput = document.querySelector("input");
+
+  //Onerilen yemeklerden birisine tiklanma durumu.
+  //Sadece tek yemek listelenmesi beklenir.
+  suggestionElement.addEventListener("click", handleGetSuggestedMeal);
+
+  function handleGetSuggestedMeal(event) {
+    //Onerilen yemek ismini al
+    const searchTerm = event.target.textContent;
+
+    //Sadece onerilen yemegi gosterecek sekilde arama yap
+    fuzzySearch(searchTerm, 1).then((meal) => {
+      createSearchResult(meal);
+      searchInput.value = "";
+      removeSuggestionList();
+    });
+  }
 
   return suggestionElement;
+}
+
+//Eger oneri listesi (eger varsa) sil.
+//Arama durumlarinda kaldirmak veya oneri durumunda ust uste ayni listeden DOM uzerinde yer kaplamamasi icin.
+export function removeSuggestionList() {
+  const suggestionsList = document.getElementById("JSsuggestionsList");
+  if (suggestionsList) {
+    suggestionsList.remove();
+  }
 }
